d9e921f7e02a94a1d7f9fe76e03b6a63ed4f4d89
AndrewScholer <ascholer@chemeketa.edu>
2019-05-23 16:40:17 -0700

    Initial Files

---
+++ IndexMap.cpp
@@ -0,0 +1,10 @@
+//----------------------------------------------------------
+// CS260 Assignment Starter Code
+// Copyright Andrew Scholer (ascholer@chemeketa.edu)
+// Neither this code, nor any works derived from it
+//    may not be republished without approval.
+//----------------------------------------------------------
+
+#include "IndexMap.h"
+
+using namespace std;
